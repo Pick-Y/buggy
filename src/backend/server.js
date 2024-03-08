98b895917d425a07781e0055fe8a14564299f9a6
Pick-Y <a.scarcella.as@gmail.com>
2024-03-08 22:02:02 +1100

    created backend folder

---
+++ src/backend/server.js
@@ -0,0 +1,14 @@
+// backend/server.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5001; // Set the port for the server
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
