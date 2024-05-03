c45ee90791450160b248daf6e3f3ec39f1436334
Pick-Y <a.scarcella.as@gmail.com>
2024-05-03 17:55:45 +1000

    Created post request: create-account

--- src/backend/server.js
+++ src/backend/server.js
@@ -8,9 +8,22 @@ const bodyParser = require('body-parser');
 
 app.use(cors());
 app.use(bodyParser.json());
+
 // Define a simple route
 app.get('/', (req, res) => {
   res.send('Hello from the backend!');
+});
+
+
+app.post('/create-account', (req, res) => {
+  // Extract data from the request body
+  const data = req.body;
+
+  // Do something with the data (e.g., save it to a database)
+  console.log('Received data:', data);
+
+  // Send a response
+  res.status(200).send('Data received successfully');
 });
 
 // Start the server
