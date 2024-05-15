948403582e3bd0a574b9646ece2799c11471609f
Pick-Y <a.scarcella.as@gmail.com>
2024-05-15 19:02:10 +1000

    Implemented fade in fade out features along with flicking between form pages with transform

--- src/pages/CreateAccountForm.jsx
+++ src/pages/CreateAccountForm.jsx
@@ -1,39 +1,85 @@
 import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Grid, Button, Container, Box } from "@mui/material";
+import styled from "styled-components";
 import Typography from "@mui/material/Typography";
 import { useFormik } from "formik";
 import {
   validationSchemaObject,
   createAccount,
 } from "../utils/createAccountFormValidationUtils";
+import { keyframes } from "styled-components";
 import AccountCreatedModal from "../components/AccountCreated";
 import { useNavigate } from "react-router-dom";
-
 import PersonalInfo from "../components/backPackerRegistrationComponents/BackPackerPersonalInfo";
+import EducationInfo from "../components/backPackerRegistrationComponents/BackPackerEducation";
+import ButtonPreviousNext from "../components/formButtonsPreviousNext.jsx";
 //Sets the color of the error text red
 const colorError = "red";
 
+// Define keyframes for animation
+
+const fadeOutRight = keyframes`
+from {
+  opacity: 1;
+  transform: translate(0);
+}
+to {
+  transform: translateX(50px);
+  opacity: 0;
+}
+`;
+
+const fadeOutLeft = keyframes`
+from {
+  opacity: 1;
+  transform: translate(0);
+}
+to {
+  transform: translateX(-50px);
+  opacity: 0;
+}
+`;
+
+
+
+const FirstDiv = styled.div`
+  background-color: '';
+  animation-duration: 0.2s;
+  animation-name: ${(props) => (props.buttonLeftClicked ? fadeOutLeft : (props.buttonRightClicked ? fadeOutRight: 'none'))};
+`;
+
 const CreateAccountForm = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isVisible, setIsVisible] = useState(true);
-  const navigate = useNavigate();
 
-  // React.useEffect(() => {
-  //   document.body.style.overflow = 'hidden';
-  //   return () => {
-  //     document.body.style.overflow = ''; // Reset overflow when component unmounts
-  //   };
-  // }, []);
+  
+  const pages = [<PersonalInfo />, <EducationInfo />]
+  const [buttonLeftClicked, setButtonLeftClicked] = useState(true);
+  const [buttonRightClicked, setButtonRightClicked] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
 
-  const handleFadeOut = () => {
-    setIsVisible(false);
+  //const navigate = useNavigate();
+
+  const handleNext = () => {
+    setButtonLeftClicked(false);
+    setTimeout(() => {
+      setButtonLeftClicked(true); // Set clicked state to true after a short delay
+    }, 10);
+    if(currentPage < pages.length - 1){
+      setCurrentPage(currentPage + 1)
+    }
+
+    
   };
 
-  const handleClose = () => {
-    console.log("In onclose");
-    setIsModalOpen(false);
-    navigate("/login-page");
+  const handlePrevious = () => {
+    setButtonLeftClicked(false);
+    setButtonRightClicked(false);
+    setTimeout(() => {
+      setButtonRightClicked(true); // Set clicked state to true after a short delay
+    }, 10);
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
   };
 
   const formik = useFormik({
@@ -41,18 +87,20 @@ const CreateAccountForm = () => {
       firstname: "",
       lastname: "",
       email: "",
+      education: "",
       password: "",
     },
     validationSchema: validationSchemaObject,
     onSubmit: (values) => {
       //HANDLE SUBMIT
-
+      console.log(values)
       createAccount(values)
         .then((response) => {
           console.log("Account created successfully:", response);
           // Handle successful response
           if (response.status === 200) {
-            setIsModalOpen(true);
+            //here, it should redirect to the user account page
+
           }
         })
         .catch((error) => {
@@ -69,138 +117,68 @@ const CreateAccountForm = () => {
       <Typography variant="h5" align="center" gutterBottom>
         Create an Account
       </Typography>
-      {isModalOpen ? (
-        //This opens a modal which gives confirmation of the account being created
-        <AccountCreatedModal open={isModalOpen} onClose={handleClose} />
-      ) : (
-        <Container
-          maxWidth={false}
-          sx={{ bgcolor: "purple", overflow: "hidden", marginTop: "40px" }}
+      <Container
+        maxWidth={false}
+        sx={{ bgcolor: "purple", overflow: "hidden", marginTop: "40px" }}
+      >
+        Second Container
+        <Box
+          sx={{
+            display: "flex",
+            "@media (max-width: 600px)": {
+              flexDirection: "column",
+            },
+            justifyContent: "center",
+            width: "90%",
+            height: "70vh",
+            margin: "auto",
+
+            marginBottom: "50px",
+          }}
         >
-          Second Container
-          <Box
-            sx={{
-              display: "flex",
-              "@media (max-width: 600px)": {
-                flexDirection: "column",
-              },
-              justifyContent: "center",
-              width: "90%",
-              height: "70vh",
-              margin: "auto",
+          <Box sx={{ bgcolor: "yellow", width: "50%", height: "100vh" }}></Box>
+          <Box sx={{ bgcolor: "#e2fdff", width: "50%", height: "100vh" }}>
+            <form onSubmit={formik.handleSubmit} autoComplete="off">
+              <FirstDiv buttonLeftClicked={buttonLeftClicked} buttonRightClicked={buttonRightClicked}>
+                {currentPage === 0 ? (
+                  <PersonalInfo
+                    firstname={formik.firstname}
+                    lastname={formik.lastname}
+                    email={formik.email}
+                    password={formik.password}
+                    handleChange={formik.handleChange}
+                    errors={formik.errors}
+                    touched={formik.touched}
+                    colorError={colorError}
+                  ></PersonalInfo>
+                ) : currentPage === 1 ? (
+                  <EducationInfo
+                    education={formik.education}
+                    handleChange={formik.handleChange}
+                    errors={formik.errors}
+                    touched={formik.touched}
+                    colorError={colorError}
+                  ></EducationInfo>
+                ) : null}
+              </FirstDiv>
 
-              marginBottom: "50px",
-            }}
-          >
-            <Box
-              sx={{ bgcolor: "yellow", width: "50%", height: "100vh" }}
-            ></Box>
-            <Box sx={{ bgcolor: "#e2fdff", width: "50%", height: "100vh" }}>
-              <form onSubmit={formik.handleSubmit} autoComplete="off">
-                <PersonalInfo 
-                  firstname={formik.firstname}
-                  lastname={formik.lastname} 
-                  email={formik.email}
-                  password={formik.password}
-                  handleChange={formik.handleChange}
-                  errors={formik.errors}
-                  touched={formik.touched}
-                  colorError={colorError}
-                  >
-                  
-                </PersonalInfo>
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="primary"
-                  style={{ marginTop: 20 }}
-                >
-                  Create Account
-                </Button>
-              </form>
-            </Box>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                style={{ marginTop: 20 }}
+              >
+                Create Account
+              </Button>
+            </form>
+            <ButtonPreviousNext
+              handleNext={handleNext}
+              handlePrevious={handlePrevious}
+            ></ButtonPreviousNext>
           </Box>
-          {/* <form onSubmit={formik.handleSubmit} autoComplete="off">
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                type="text"
-                id="name"
-                name="name"
-                label="First Name"
-                variant="outlined"
-                onChange={formik.handleChange}
-                value={formik.values.name}
-              />
-              {formik.touched.password && formik.errors.name ? (
-                <div style={{ color: colorError }}>{formik.errors.name}</div>
-              ) : null}
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                fullWidth
-                type="text"
-                id="lastname"
-                name="lastname"
-                label="Last Name"
-                variant="outlined"
-                onChange={formik.handleChange}
-                value={formik.values.lastName}
-              />
-              {formik.touched.password && formik.errors.lastname ? (
-                <div style={{ color: colorError }}>
-                  {formik.errors.lastname}
-                </div>
-              ) : null}
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                type="text"
-                id="email"
-                name="email"
-                label="Email"
-                variant="outlined"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-              />
-              {formik.touched.password && formik.errors.email ? (
-                <div style={{ color: colorError }}>{formik.errors.email}</div>
-              ) : null}
-            </Grid>
-
-            <Grid item xs={12}>
-              <TextField
-                fullWidth
-                type="text"
-                id="password"
-                name="password"
-                label="Password"
-                variant="outlined"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-              />
-              {formik.touched.password && formik.errors.password ? (
-                <div style={{ color: colorError }}>
-                  {formik.errors.password}
-                </div>
-              ) : null}
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            style={{ marginTop: 20 }}
-          >
-            Create Account
-          </Button>
-        </form> */}
-        </Container>
-      )}
+        </Box>
+      </Container>
     </Container>
   );
 };
