095e02640ce499da8cefc8f41f197833ebc804f3
Pick-Y <a.scarcella.as@gmail.com>
2024-05-25 00:54:01 +1000

    moved isFormValid and setIsFormValid into CreateAccount

--- src/components/backPackerRegistrationComponents/BackPackerFormFirstPage.jsx
+++ src/components/backPackerRegistrationComponents/BackPackerFormFirstPage.jsx
@@ -1,23 +1,42 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import { Grid, Button, Container, Box } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { boolean, object } from "yup";
 
 //Sets the color of the error text red
 const BackPackerFormFirstPage = ({
   firstname,
   lastname,
   email,
-  password,
+  //password,
   handleChange,
   errors,
+  isFormValid,
+  setIsFormValid,
   touched,
   colorError,
 }) => {
-  console.log('this should be email: ' + errors.email)
-  console.log(touched.email)
-  console.log(touched.lastname)
-  return (
+
+
+
+
+// const [formErrors,setFormErrors] = useState({})
+// const [isFormValid, setIsFormValid] = useState(false)
+
+
+
+
+useEffect(() =>{
+    //console.log(Object.keys(errors).length)
+    // setFormErrors(errors)
+    // setIsFormValid(Object.keys(errors).length === 0)
+    //console.log(isFormValid)
+    const isValid = (Object.keys(errors).length === 0)
+    setIsFormValid(isValid)
+  },[errors,setIsFormValid])
+  
+return (
     <>
       <Grid container spacing={2}>
         <Grid item xs={12} sm={12}>
@@ -52,34 +71,20 @@ const BackPackerFormFirstPage = ({
           )}
         </Grid>
         <Grid item xs={12}>
-        
-
-          {/* <TextField
+          <TextField
             fullWidth
             type="text"
             id="email"
             name="email"
             label="Email"
-            variant="outlined"
             onChange={handleChange}
             value={email}
-          />
-          {touched.email && errors.email && (
+          ></TextField>
+          {errors.email ? (
             <div style={{ color: colorError }}>{errors.email}</div>
-          )} */}
-          <TextField
-          fullWidth
-          type="text"
-          id='email'
-          name="email"
-          label='Email'
-          onChange={handleChange}
-          value={email}
-
-          >
-            
-          </TextField>
-          {errors.email ?  (<div style={{ color: colorError }}>{errors.email}</div>) : 'null'}
+          ) : (
+            ""
+          )}
         </Grid>
 
         <Grid item xs={12}>
@@ -101,7 +106,5 @@ const BackPackerFormFirstPage = ({
     </>
   );
 };
-
-
 
 export default BackPackerFormFirstPage;
