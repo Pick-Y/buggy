b91c522b7f1d3dfc81c14267681477a65f879ca2
Pick-Y <a.scarcella.as@gmail.com>
2024-06-28 20:56:27 +1000

    Added fourth page for password

--- src/pages/CreateAccountForm.jsx
+++ src/pages/CreateAccountForm.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import TextField from "@mui/material/TextField";
-import { Grid, Button, Container, Box } from "@mui/material";
+import { Grid, Button, Container, Box, Switch } from "@mui/material";
 import styled from "styled-components";
 import Typography from "@mui/material/Typography";
 import { useFormik } from "formik";
@@ -11,8 +11,18 @@ import {
 import { keyframes } from "styled-components";
 import AccountCreatedModal from "../components/AccountCreated";
 import { useNavigate } from "react-router-dom";
-import BackPackerFormFirstPage from "../components/backPackerRegistrationComponents/BackPackerFormFirstPage.jsx";
-import BackPackerFormSecondPage from "../components/backPackerRegistrationComponents/BackPackerFormSecondPage.jsx";
+import BackPackerFormFirstPage, {
+  firstPageFields,
+} from "../components/backPackerRegistrationComponents/BackPackerFormFirstPage.jsx";
+import BackPackerFormSecondPage, {
+  secondPageFields,
+} from "../components/backPackerRegistrationComponents/BackPackerFormSecondPage.jsx";
+import BackPackerFormThirdPage, {
+  thirdPageFields,
+} from "../components/backPackerRegistrationComponents/BackPackerFormThirdPage.jsx";
+import BackPackerFormFourthPage, {
+  fourthPageFields,
+} from "../components/backPackerRegistrationComponents/BackPackerFormFourthPage.jsx";
 import EducationInfo from "../components/backPackerRegistrationComponents/BackPackerEducation";
 import ButtonPreviousNext from "../components/formButtonsPreviousNext.jsx";
 //Sets the color of the error text red
@@ -56,47 +66,32 @@ const FirstDiv = styled.div`
 `;
 
 const CreateAccountForm = () => {
-  const pages = [<BackPackerFormFirstPage />, <EducationInfo />];
+  const pages = [
+    <BackPackerFormFirstPage />,
+    <BackPackerFormSecondPage />,
+    <BackPackerFormThirdPage />,
+    <BackPackerFormFourthPage />,
+  ];
   const [buttonLeftClicked, setButtonLeftClicked] = useState(true);
   const [buttonRightClicked, setButtonRightClicked] = useState(true);
   const [currentPage, setCurrentPage] = useState(0);
   const [isFormValid, setIsFormValid] = useState(false);
-
+  const [singlePageErrors, setSinglePageErrors] = useState({});
   //const navigate = useNavigate();
 
-  const handleNext = () => {
-    if (isFormValid) {
-      setButtonLeftClicked(false);
-      setTimeout(() => {
-        setButtonLeftClicked(true); // Set clicked state to true after a short delay
-      }, 10);
-      if (currentPage < pages.length - 1) {
-        setCurrentPage(currentPage + 1);
-      }
-    }else{
-      alert("fill the form")
-    }
-  };
-
-  const handlePrevious = () => {
-    setButtonLeftClicked(false);
-    setButtonRightClicked(false);
-    setTimeout(() => {
-      setButtonRightClicked(true); // Set clicked state to true after a short delay
-    }, 10);
-    if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
   const formik = useFormik({
     initialValues: {
       firstname: "",
       lastname: "",
       email: "",
+      nationality: "",
+      age: "",
+      gender: "",
+      education: "",
+      currentvisa: "",
+      workrights: "",
       password: "",
-      // dob:"",
-      // education: "",
-      // password: "",
+      confirmpassword: "",
     },
     validationSchema: validationSchemaObject,
     onSubmit: (values) => {
@@ -118,12 +113,79 @@ const CreateAccountForm = () => {
     },
   });
 
-  const dynamicErrors = {};
-  Object.keys(formik.values).forEach((field) => {
-    if (formik.errors[field]) {
-      dynamicErrors[field] = formik.errors[field];
+  const singlePageErrorFilter = useCallback(
+    (pageFields) => {
+      const singleErrorFieldObj = {};
+
+      pageFields.forEach((key) => {
+        if (key in formik.errors) {
+          console.log(formik.errors[key]);
+          singleErrorFieldObj[key] = formik.errors[key];
+        }
+      });
+
+      return singleErrorFieldObj;
+    },
+    [formik.errors]
+  );
+
+  const validateSinglePage = useCallback(() => {
+    switch (currentPage) {
+      case 0:
+        setSinglePageErrors(singlePageErrorFilter(firstPageFields));
+        break;
+      case 1:
+        setSinglePageErrors(singlePageErrorFilter(secondPageFields));
+        break;
+      case 2:
+        setSinglePageErrors(singlePageErrorFilter(thirdPageFields));
+        break;
+      case 3:
+        setSinglePageErrors(singlePageErrorFilter(fourthPageFields));
+        break;
+      default:
     }
-  });
+  }, [currentPage, singlePageErrorFilter]);
+
+  useEffect(() => {
+    validateSinglePage();
+  }, [currentPage, validateSinglePage]);
+
+  // const dynamicErrors = {};
+  // Object.keys(formik.values).forEach((field) => {
+  //   if (formik.errors[field]) {
+  //     dynamicErrors[field] = formik.errors[field];
+  //   }
+  // });
+
+  const handleNext = () => {
+    //check if the fields have been filled
+
+    //if they have not been fill
+    //pass errors for the single page
+    if (isFormValid) {
+      setButtonLeftClicked(false);
+      setTimeout(() => {
+        setButtonLeftClicked(true); // Set clicked state to true after a short delay
+      }, 10);
+      if (currentPage < pages.length - 1) {
+        setCurrentPage(currentPage + 1);
+      }
+    } else {
+      alert("fill the form");
+    }
+  };
+
+  const handlePrevious = () => {
+    setButtonLeftClicked(false);
+    setButtonRightClicked(false);
+    setTimeout(() => {
+      setButtonRightClicked(true); // Set clicked state to true after a short delay
+    }, 10);
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
 
   return (
     <Container maxWidth="lg" sx={{ overflow: "hidden", height: "100hv" }}>
@@ -187,14 +249,47 @@ const CreateAccountForm = () => {
                     email={formik.email}
                     password={formik.password}
                     handleChange={formik.handleChange}
-                    errors={dynamicErrors}
+                    errors={singlePageErrors}
                     touched={formik.touched}
                     colorError={colorError}
                     isFormValid={isFormValid}
                     setIsFormValid={setIsFormValid}
-                  ></BackPackerFormFirstPage>
+                  />
                 ) : currentPage === 1 ? (
-                  <div>moved to second page`</div>
+                  <BackPackerFormSecondPage
+                    nationality={formik.nationality}
+                    age={formik.age}
+                    gender={formik.gender}
+                    handleChange={formik.handleChange}
+                    errors={singlePageErrors}
+                    touched={formik.touched}
+                    colorError={colorError}
+                    isFormValid={isFormValid}
+                    setIsFormValid={setIsFormValid}
+                  />
+                ) : currentPage === 2 ? (
+                  <BackPackerFormThirdPage
+                    education={formik.education}
+                    currentvisa={formik.currentvisa}
+                    workrights={formik.workrights}
+                    handleChange={formik.handleChange}
+                    errors={singlePageErrors}
+                    touched={formik.touched}
+                    colorError={colorError}
+                    isFormValid={isFormValid}
+                    setIsFormValid={setIsFormValid}
+                  />
+                ) : currentPage === 3 ? (
+                  <BackPackerFormFourthPage
+                    password={formik.password}
+                    confirmpassword={formik.confirmpassword}
+                    handleChange={formik.handleChange}
+                    errors={singlePageErrors}
+                    touched={formik.touched}
+                    colorError={colorError}
+                    isFormValid={isFormValid}
+                    setIsFormValid={setIsFormValid}
+                  />
                 ) : null}
               </FirstDiv>
 
