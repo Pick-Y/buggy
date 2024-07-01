22c8babe2a9d5aecc6ad8d40fce60e28295e8cb6
Pick-Y <a.scarcella.as@gmail.com>
2024-07-01 18:56:10 +1000

    inserted {children}

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home';
+import NavBar from './components/NavBar';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers';
+import { Children } from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        {Children}
+        {/* <Home />
+        <NavBar /> */}
+      </LocalizationProvider>
+
     </div>
   );
 }
