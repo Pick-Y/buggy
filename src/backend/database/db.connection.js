8c0abb5dd5e71bad1d4fda877ea23c7842cfce84
Pick-Y <a.scarcella.as@gmail.com>
2024-07-14 19:42:35 +1000

    created database connection

---
+++ src/backend/database/db.connection.js
@@ -0,0 +1,18 @@
+// This establishes the connection to the database
+
+const { Sequelize } = require('sequelize');
+const dbConfig = require('./db.config');
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  port: dbConfig.PORT,
+  dialect: dbConfig.dialect,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
+
+module.exports = sequelize;
