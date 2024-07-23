f5f908a28b86f0e7e0ce55379fef640771f70353
Pick-Y <a.scarcella.as@gmail.com>
2024-07-23 23:01:32 +1000

    Replaced fields with address, postalcode and industry for the JobPoster

---
+++ src/components/farmerRegiStrationComponents/JobPosterFormSecondPage.jsx
@@ -0,0 +1,158 @@
+import React, { useEffect, useMemo } from "react";
+import TextField from "@mui/material/TextField";
+import {
+  Grid,
+  // MenuItem,
+  // Radio,
+  // RadioGroup,
+  // FormControl,
+  // FormControlLabel,
+  // FormLabel,
+} from "@mui/material";
+import countryList from "react-select-country-list";
+
+export const secondPageFields = ["address", "postalcode", "state"];
+
+//       address: "",
+//       postalcode: "",
+//       state: "",
+//       phone: "",
+//       email: "",
+//       contactperson: "",
+//       password: "",
+//       confirmpassword: "",
+// const ageRanges = [
+//   { label: "18-23", value: "18-23" },
+//   { label: "24-26", value: "24-26" },
+//   { label: "27-33", value: "27-33" },
+// ];
+
+const JobPosterFormSecondPage = ({
+  values,
+  handleChange,
+  errors,
+  setIsFormValid,
+  touched,
+  colorError,
+}) => {
+  const countries = useMemo(() => countryList().getData(), []);
+
+  console.log(errors);
+  useEffect(() => {
+    const isValid = Object.keys(errors).length === 0;
+    setIsFormValid(isValid);
+  }, [errors, setIsFormValid]);
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12}>
+          <TextField
+            fullWidth
+            select
+            type="text"
+            id="address"
+            name="address"
+            label="Address"
+            variant="outlined"
+            onChange={handleChange}
+            value={values.address}
+          >
+            {/* {countries.map((option) => (
+              <MenuItem key={option.value} value={option.label}>
+                {option.label}
+              </MenuItem>
+            ))} */}
+          </TextField>
+          {touched.address && errors.address && (
+            <div style={{ color: colorError }}>{errors.address}</div>
+          )}
+        </Grid>
+
+        <Grid item xs={12} sm={12}>
+          <TextField
+            fullWidth
+            select
+            type="text"
+            id="postalcode"
+            name="postalcode"
+            label="Postal Code"
+            variant="outlined"
+            onChange={handleChange}
+            value={values.postalcode}
+          >
+            {/* {ageRanges.map((option) => (
+              <MenuItem key={option.value} value={option.label}>
+                {option.label}
+              </MenuItem>
+            ))} */}
+          </TextField>
+          {touched.postalcode && errors.postalcode && (
+            <div style={{ color: colorError }}>{errors.postalcode}</div>
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            fullWidth
+            select
+            type="text"
+            id="state"
+            name="state"
+            label="State"
+            variant="outlined"
+            onChange={handleChange}
+            value={values.state}
+          ></TextField>
+          {touched.state && errors.state && (
+            <div style={{ color: colorError }}>{errors.state}</div>
+          )}
+          {/* <FormControl component="fieldset">
+            <FormLabel component="legend">Gender</FormLabel>
+            <RadioGroup
+              row
+              aria-label="gender"
+              name="gender"
+              value={values.gender}
+              onChange={handleChange}
+            >
+              <FormControlLabel value="male" control={<Radio />} label="Male" />
+              <FormControlLabel
+                value="female"
+                control={<Radio />}
+                label="Female"
+              />
+              <FormControlLabel
+                value="other"
+                control={<Radio />}
+                label="Other"
+              />
+            </RadioGroup>
+          </FormControl>
+          {touched.gender && errors.gender ? (
+            <div style={{ color: colorError }}>{errors.gender}</div>
+          ) : (
+            ""
+          )} */}
+        </Grid>
+
+        <Grid item xs={12}>
+          {/* <TextField
+            fullWidth
+            type="text"
+            id="password"
+            name="password"
+            label="Password"
+            variant="outlined"
+            onChange={handleChange}
+            value={password}
+          />
+          {errors.password ? (
+            <div style={{ color: colorError }}>{errors.password}</div>
+          ) : null} */}
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default JobPosterFormSecondPage;
